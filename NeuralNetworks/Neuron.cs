911f87b1da0aec8f66f8c942f2a5632e95d2c19d
Devil-in-I <maxim_popov_98@mail.ru>
2021-03-27 17:48:06 +0600

    Добавьте файлы проекта.

---
+++ NeuralNetworks/Neuron.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworks
+{
+    public class Neuron
+    {
+        public List<double> Weights { get; }
+
+        public NeuronType NeuronType { get; }
+        public double Output { get; private set; }
+
+        public Neuron(int inputCount, NeuronType neuronType = NeuronType.normal)
+        { 
+            NeuronType = neuronType;
+            Weights = new List<double>();
+
+            for (int i = 0; i < inputCount; i++)
+            {
+                Weights.Add(1);
+            }
+        }
+
+        public double FeedForward(List<double> inputs)
+        {
+            if (inputs == null || inputs.Count <= 0)
+                return -1;
+
+            var sum = 0.0;
+            for (int i = 0; i < inputs.Count; i++)
+            {
+                sum += inputs[i] + Weights[i];
+            }
+            Output = Sigmoid(sum);
+            return Output;
+        }
+        private double Sigmoid(double x)
+        {
+            var result = 1 / (1.0 + Math.Pow(Math.E, -x));
+            return result;
+        }
+        public override string ToString()
+        {
+            return Output.ToString();
+        }
+    }
+}
---
+++ NeuralNetworks/NeuronType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworks
+{
+    public enum NeuronType
+    {
+        input  =  0,
+        normal =  1, 
+        output =  2
+    }
+}
